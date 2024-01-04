075ddcadc4881e05022c455fca222d6d48541819
O-white1 <white.o3@stu.janesville.k12.wi.us>
2024-01-04 12:56:18 -0600

    12:55 1/4/24 GD_Final

--- Crimson_Crypt.gd
+++ Crimson_Crypt.gd
@@ -1,8 +1,18 @@
 extends Node2D
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	$FirePillar1/AnimatedSprite2D.play("default")
+	$FirePillar2/AnimatedSprite2D.play("default")
+	$FirePillar3/AnimatedSprite2D.play("default")
+	$FirePillar4/AnimatedSprite2D.play("default")
+	$FirePillar1/AnimatedSprite2D2.play("default")
+	$FirePillar2/AnimatedSprite2D2.play("default")
+	$FirePillar3/AnimatedSprite2D2.play("default")
+	$FirePillar4/AnimatedSprite2D2.play("default")
+	$FirePillar1/AnimatedSprite2D3.play("default")
+	$FirePillar2/AnimatedSprite2D3.play("default")
+	$FirePillar3/AnimatedSprite2D3.play("default")
+	$FirePillar4/AnimatedSprite2D3.play("default")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
